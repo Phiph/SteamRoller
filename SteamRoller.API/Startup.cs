00fd3cb2ae61bbfe14a2262e2547ec52bb48eba4
Phill Morton <me@phillmorton.co.uk>
2021-07-25 23:14:43 +0100

    Implement Controllers With NSwag

--- SteamRoller.API/Startup.cs
+++ SteamRoller.API/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,10 +28,10 @@ namespace SteamRoller.API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SteamRoller.API", Version = "v1" });
-            });
+
+             //## NSwag
+            services.AddOpenApiDocument(d => d.Title = "SteamRoller.API");
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +40,13 @@ namespace SteamRoller.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SteamRoller.API v1"));
+                
             }
+
+            
+            //## NSwag
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             //app.UseHttpsRedirection();
 
