eb19ec48b61d5e213bdf1b4fc3b6e46009585c21
Phill Morton <me@phillmorton.co.uk>
2021-07-25 23:15:00 +0100

    move player to Index page

--- Electron/SteamRoller.Client/Pages/Index.cshtml.cs
+++ Electron/SteamRoller.Client/Pages/Index.cshtml.cs
@@ -5,12 +5,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SteamRoller.Core;
+using SteamRoller.Client.Services;
 
 namespace SteamRoller.Client.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+
+        public Game SelectedGame;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +24,22 @@ namespace SteamRoller.Client.Pages
 
         public void OnGet()
         {
+            SteamLibraryService steam = new SteamLibraryService();
 
+            Console.WriteLine("The app has loaded, no flaky code yet.");
+            Console.WriteLine("Initialising");
+            //Load up Libraries
+            // and get installed games. 
+            Console.WriteLine($" you have {steam.Locations.Count} steam libs");
+            Console.WriteLine("Welcome to the Steam Pick yer game - innit");
+            Console.WriteLine($" you have {steam.Library.Games.Count} games installed");
+            Console.WriteLine($" you have {steam.Library.Games.Where(x => x.StateFlags == "4").Count()} state 4 (Ready to play)");
+            Console.WriteLine($" you have {steam.Library.Games.Where(x => x.StateFlags == "6").Count()} state 6 (Update scheduled)");
+
+            var random = new Random();
+            int index = random.Next(steam.Library.Games.Count);
+            SelectedGame = steam.Library.Games[index];
+            Console.WriteLine(SelectedGame.Name);
         }
     }
 }
