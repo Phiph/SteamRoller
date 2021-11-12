8b51a5e21fbd55f110315ec162a76d5a193bc82d
Phill Morton <phill.morton@theaccessgroup.com>
2021-11-12 13:36:45 +0000

    roll back dapr upgrade.

--- src/Client/SteamRoller.Client/Program.cs
+++ src/Client/SteamRoller.Client/Program.cs
@@ -16,7 +16,7 @@ namespace SteamRoller.Client
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+         
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
